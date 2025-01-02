fa09fc0a25717ef26e90be73f6930180f746faaa
Changwan Kim <chakim@c2r4s3.42gyeongsan.kr>
2025-01-02 10:07:29 +0900

    dashboard-update

--- lua/config/42/init.lua
+++ lua/config/42/init.lua
@@ -3,8 +3,8 @@
 -- NOTE: If you are from 42, **you might want to edit these** :)
 --
 -- 42 Global Variables.
-vim.g.user42 = 'SET YOUR USER UP'
-vim.g.mail42 = 'SET YOUR EMAIL UP'
+vim.g.user42 = 'chakim'
+vim.g.mail42 = 'chakim@student.42gyeongsan.kr'
 
 require("norminette").setup({
 	runOnSave = true,
